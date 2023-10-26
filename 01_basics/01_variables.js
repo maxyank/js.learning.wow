214027db28f9d60181ff58d5a2f8e3f0196172f6
maxyank <117353856+maxyank@users.noreply.github.com>
2023-10-26 15:38:38 +0000

    datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "Mayankshukla547019@gmail.com"
+var  accountPassword = "12345"
+accountCity = "jaipur" 
+ 
+console.log(accountId);
+
+console.table([accountEmail , accountId , accountPassword , accountCity])
+
+
+// type of data type
+console.log(typeof(accountId))
